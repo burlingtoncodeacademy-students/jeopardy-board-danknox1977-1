be602e5d482bd1d2ff5fa9eef1f0dfaf1c893d5f
danknox1977 <127804873+danknox1977@users.noreply.github.com>
2023-05-06 15:11:39 -0400

    Basically addressed feedback from 1st submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
+
+
+const button = document.getElementById(qBox)
+
+const dsplyAnswr = addEventListener("click", ())
+
+const clearButton = document.createElement("button");
+clearButton.innerText = "Delete";
+clearButton.addEventListener("click", async () => {
+  const response = await fetch(BASEURL + "/skewps/" + skewp.id, {
+    
+  });
+  console.log(await response.text());
