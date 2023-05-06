333ef3a6f0ed5f3ee861c8a6ae7fa2026961c6da
danknox1977 <127804873+danknox1977@users.noreply.github.com>
2023-05-06 16:01:08 -0400

    added class extensions to gameboard cards

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,24 @@ import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
 
 
-const button = document.getElementById(qBox)
 
-const dsplyAnswr = addEventListener("click", ())
+// let currentPlayer = p1
 
-const clearButton = document.createElement("button");
-clearButton.innerText = "Delete";
-clearButton.addEventListener("click", async () => {
-  const response = await fetch(BASEURL + "/skewps/" + skewp.id, {
+// let p1
+
+// let p2
+
+
+const gameboard = document.getElementsByClassName("gameboard");
+
+console.log(gameboard)
+
+// const dsplyAnswr = addEventListener("click", ())
+
+// const clearButton = document.createElement("button");
+// clearButton.innerText = "Delete";
+// clearButton.addEventListener("click", async () => {
+//   const response = await fetch(BASEURL + "/skewps/" + skewp.id, {
     
-  });
-  console.log(await response.text());
+//   });
+//   console.log(await response.text());
